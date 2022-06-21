85b6a47ee7f177cf115c15665eaae71a09642028
Gerry Guinardi <73393827+rousin1408@users.noreply.github.com>
2022-06-21 15:13:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask, request, jsonify
 
 import language_tool_python
 app = Flask(__name__)
-@app.route('/spelling', methods=['POST'])
+@app.route('/spelling', methods=['GET'])
 def spelling():
-    if request.is_json:
-        data = request.get_json()
+    return "tester"
+#     if request.is_json:
+#         data = request.get_json()
 
-        typo = data["sentence"]
-        tool = language_tool_python.LanguageToolPublicAPI('en-US')
-        final = tool.correct(typo)
-        return print(final)
+#         typo = data["sentence"]
+#         tool = language_tool_python.LanguageToolPublicAPI('en-US')
+#         final = tool.correct(typo)
+#         return print(final)
        
-    return {"error": "Request must be JSON"}, 415 
+#     return {"error": "Request must be JSON"}, 415 
